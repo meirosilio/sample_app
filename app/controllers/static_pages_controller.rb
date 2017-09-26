a067bd1285a437115be2f074cbf4dae06e3a1013
Meir Rosilio <meirosilio@gmail.com>
2017-09-26 19:51:52 +0300

    Finish layout and routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,4 +13,8 @@ class StaticPagesController < ApplicationController
   def about
 
   end
+
+  def contact
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+  get '/signup', to:'users#new'
+
   root 'static_pages#home'
 
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
+  get 'static_pages/contact'
+
+
 
 end

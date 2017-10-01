61f1c20da80f6baa087e205f0b74194089482fde
Meir Rosilio <meirosilio@gmail.com>
2017-10-01 19:45:46 +0300

    Implementing basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
   before_save{self.email=email.downcase}
+  has_secure_password
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -14,7 +15,9 @@ class User < ApplicationRecord
             format: {with: VALID_EMAIL_REGEX},
             uniqueness:{case_sensitive: false}
 
-  has_secure_password
-  validates :password_digest, presence: true, length: {minimum: 6}
+
+  validates :password_digest,
+            presence: true,
+            length: {minimum: 6}
 
 end
